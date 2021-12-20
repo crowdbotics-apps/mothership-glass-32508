3e3a3d1b172a48aafd99d6065bbfd3be13363e15
Crowdbotics <team@crowdbotics.com>
2021-12-20 19:45:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "mothership_glass_32508",
+  url: "https://mothership_glass_32508.botics.co",
+  api: "https://mothership_glass_32508.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
